60888602560fc493a4c899206742c65a5f39fd56
humbledgeeks <allen@humbledgeeks.com>
2025-02-16 18:34:45 -0800

    Updated

--- o/o/o/o/o/o/Untitled3.ps1
+++
@@ -1,3 +0,0 @@
-﻿$vmnic1 = Get-VMhost | Get-VMHostNetworkAdapter -Physical -Name vmnic1
-$vs0 = Get-VirtualSwitch -VMHost $myVMHost -Name 'vSwitch0'
-$vs0 | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $vmnic1 -Confirm:$false
